e4f30c34809e53824548f5c0ef5adc383f406832
WuJiang5521 <wujiang5521@qq.com>
2020-07-13 16:44:56 +0800

    update map

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,13 +1,14 @@
 import React, {useContext} from 'react';
-import ReactEcharts from 'echarts-for-react';
+import { Map, APILoader } from '@uiw/react-baidu-map';
 import {store} from "../store";
 
 function DetailView() {
     const {state, dispatch} = useContext(store);
 
-    return <div>
-        <p>{state.count}</p>
-        <button onClick={() => dispatch({type: 'increment'})}>add</button>
+    return <div style={{ width: '100%', height: '100%' }}>
+        <APILoader akay="Yvjkj9EN8qfW4vDB6WoTfd01DYWWTzDW">
+            <Map />
+        </APILoader>
     </div>;
 }
 
